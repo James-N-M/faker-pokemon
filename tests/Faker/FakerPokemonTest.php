079640c7cfb96826b4475f3c0d4d9f741b6216be
James Moore <jamesm0025@gmail.com>
2019-09-23 22:39:49 -0400

    added a list of pokemon names, finally got basic test passing by included autoloaded class map right in the test file

--- tests/Faker/FakerPokemonTest.php
+++ tests/Faker/FakerPokemonTest.php
@@ -2,10 +2,12 @@
 
 namespace Faker\Tests\Provider;
 
+require '../../vendor/autoload.php';
+
 use Faker\Factory;
 use Faker\Generator;
-use Faker\Provider\FakerPokemon;
 use Faker\Provider\PokemonData;
+use Faker\Provider\FakerPokemon;
 use PHPUnit\Framework\TestCase;
 
 class FakerPokemonTest extends TestCase
@@ -15,15 +17,26 @@ class FakerPokemonTest extends TestCase
      */
     private $faker;
 
-    public function setUp() : void
+    public function setUp(): void
     {
         $faker = Factory::create();
         $faker->addProvider(new FakerPokemon($faker));
         $this->faker = $faker;
     }
 
-    public function testPokemon()
+    public function testTest()
     {
-        $this->assertEquals("Pikachu", $this->faker->pokemon());
+        $this->assertEquals("Hello", "Hello");
     }
+
+    public function testPokemonName()
+    {
+        $this->faker->seed(random_int(1, 9999));
+
+        $pokemonName = $this->faker->pokemonName();
+
+        $this->assertNotEmpty($pokemonName);
+    }
+
+
 }
